db5157462a7a0cb50986f23fe04c89a2d780c93f
OttoRigobello <138151969+OttoRigobello@users.noreply.github.com>
2023-07-06 20:15:51 -0300

    Atualizar o ati07.java

--- Apostila1/ati07.java
+++ Apostila1/ati07.java
@@ -1,7 +1,13 @@
 public class ati07 {
-    public static void main(String[]  args){
-        String  tosse; bolacha;
-    System.out.println("ajudem a familia do otti");
-    Scanner ooo = new Scanner(System.in);
-    }
+public static void main(String[]  args){
+
+System.out.println("          $          ");
+System.out.println("        $ $ $        ");
+System.out.println("      $ $ $ $ $      ");
+System.out.println("    $ $ $ $ $ $ $    ");
+System.out.println("  $ $ $ $ $ $ $ $ $  ");
+    
+ 
+
+ }
 }
