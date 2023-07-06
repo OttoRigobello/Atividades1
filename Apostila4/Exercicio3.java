d5a943bc98ae96978e7de5d42a0e3f4141a405ac
OttoRigobello <138151969+OttoRigobello@users.noreply.github.com>
2023-07-06 16:43:09 -0300

    Update Exercicio3.java

--- Apostila4/Exercicio3.java
+++ Apostila4/Exercicio3.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
-public class Exercicio3 {
-    public static void main(String[] args) {
+ import java.util.Scanner;
+        public class Exercicio3 {
+        public static void main(String[] args) {
         Scanner numint = new Scanner(System.in);
-        System.out.println("Digite um numero: ");
+        System.out.print("Digite um numero: ");
         int n1 = numint.nextInt();
-        System.out.println("Digite um numero: ");
+        System.out.print("Digite um numero: ");
         int n2 = numint.nextInt();
-        int 
-    }
-}
+        String conc = Integer.toString(n1) + Integer.toString(n2);
+        System.out.println("Concatenação dos números: " + conc);
+        }
+        }
+        
