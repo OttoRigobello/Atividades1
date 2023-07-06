895e774b33d4f0870f8ad8cab6fb2ff54e6ac16b
OttoRigobello <138151969+OttoRigobello@users.noreply.github.com>
2023-07-06 20:20:05 -0300

    Atualizar o Exercicio17.java

--- Apostila2/Exercicio17.java
+++ Apostila2/Exercicio17.java
@@ -3,7 +3,8 @@
         public static void main(String[] args) {
         Scanner Welcome = new Scanner(System.in);
         System.out.print("Digite seu nome: ");
-        String name = Welcome.nextLine();
-        System.out.println("\nOlá "+name+", seja bem vindo(a)! (ɔ◔‿◔)ɔ ♥");
-        }
-        }
+        String nome = Welcome.nextLine();
+        System.out.println("\nOlá " + nome + ", seja bem vindo(a)");
+     
+   }
+   }
