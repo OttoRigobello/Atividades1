f3eb032902123b76ace3f40250c2d708628c1e83
OttoRigobello <138151969+OttoRigobello@users.noreply.github.com>
2023-07-06 22:21:11 -0300

    Update ati04.java

--- Apostila1/ati04.java
+++ Apostila1/ati04.java
@@ -1,10 +1,10 @@
-public class ati04 {
-    public static void main(String[] args) {
+        public class ati04 {
+        public static void main(String[] args) {
         System.out.println("*    *     *     *    *");
         System.out.println("*                       *");
         System.out.println("*                       *");
         System.out.println("*                       *");
         System.out.println("*    *     *    *    * ");
-
-    }
-    }
+        }
+        }
+  
