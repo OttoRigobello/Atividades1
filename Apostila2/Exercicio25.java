b00b590240a78211d1e1269d04b7640129fe37c8
OttoRigobello <138151969+OttoRigobello@users.noreply.github.com>
2023-07-06 21:05:28 -0300

    Atualizar o Exercicio25.java

--- Apostila2/Exercicio25.java
+++ Apostila2/Exercicio25.java
@@ -1,19 +1,21 @@
-        import java.util.Scanner;
-        public class Exercicio25 {
-        public static void main(String[] args) {
-        Scanner Change = new Scanner(System.in);
-        System.out.println("Digite 2 numeros abaixo com seus antigos valores");
-        int A, B, C;
-        System.out.print("Primeiro Número: ");
-        A = Change.nextInt();
-        System.out.print("Segundo Número: ");
-        B = Change.nextInt();
+import java.util.Scanner;
+
+public class Exercicio25 {
+public static void main(String[] args) {
+
+Scanner Change = new Scanner(System.in);
+
+System.out.println("Digite 2 numeros abaixo com seus antigos valores");       
+int A, B, C;
+System.out.print("Primeiro Número: ");
+A = Change.nextInt();
+System.out.print("Segundo Número: ");
+B = Change.nextInt();
         C = A;
         A = B;
         B = C;
-        System.out.println("Novo resultado de A:  "+A);
-        System.out.println("Novo resultado de B: "+B);
+System.out.println("Novo resultado de A:  " + A);
+System.out.println("Novo resultado de B: " + B);
 
-
-        }
-        }
+}
+   }
