48639a98c05f40af9bccb78e8693d79351d097ee
OttoRigobello <138151969+OttoRigobello@users.noreply.github.com>
2023-07-06 22:20:36 -0300

    Update ati03.java

--- Apostila1/ati03.java
+++ Apostila1/ati03.java
@@ -1,6 +1,5 @@
-public class ati03 {
-
-    public static void main(String[] args) {
+        public class ati03 {
+        public static void main(String[] args) {
         System.out.println("***********");
-    }
-}
+        }
+        }
